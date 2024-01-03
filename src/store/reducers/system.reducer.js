1e719887ca18e1f8d448915632378d5dd9def05b
Noam Saar <noamsaar11@gmail.com>
2024-01-03 15:26:40 +0200

    Refactor user and system reducers

--- src/store/reducers/system.reducer.js
+++ src/store/reducers/system.reducer.js
@@ -3,9 +3,9 @@ export const LOADING_DONE = 'LOADING_DONE'
 
 const initialState = {
   isLoading: false
-};
+}
 
-export function systemReducer (state = initialState, action = {}) {
+export function systemReducer(state = initialState, action = {}) {
   switch (action.type) {
     case LOADING_START:
       return { ...state, isLoading: true }
--- src/store/reducers/user.reducer.js
+++ src/store/reducers/user.reducer.js
@@ -1,33 +1,22 @@
-import { userService } from '../services/user.service.js'
+import { userService } from '../../services/user.service.js'
 
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
-export const CHANGE_COUNT = 'CHANGE_COUNT'
 export const SET_USER = 'SET_USER'
 export const SET_WATCHED_USER = 'SET_WATCHED_USER'
 export const REMOVE_USER = 'REMOVE_USER'
 export const SET_USERS = 'SET_USERS'
-export const SET_SCORE = 'SET_SCORE'
 
 const initialState = {
     count: 10,
     user: userService.getLoggedinUser(),
     users: [],
-    watchedUser : null
+    watchedUser: null
 }
 
 export function userReducer(state = initialState, action) {
     var newState = state
     switch (action.type) {
-        case INCREMENT:
-            newState = { ...state, count: state.count + 1 }
-            break
-        case DECREMENT:
-            newState = { ...state, count: state.count - 1 }
-            break
-        case CHANGE_COUNT:
-            newState = { ...state, count: state.count + action.diff }
-            break
         case SET_USER:
             newState = { ...state, user: action.user }
             break
@@ -43,14 +32,10 @@ export function userReducer(state = initialState, action) {
         case SET_USERS:
             newState = { ...state, users: action.users }
             break
-        case SET_SCORE:
-            newState = { ...state, user: { ...state.user, score: action.score } }
-            break
         default:
     }
     // For debug:
     // window.userState = newState
     // console.log('State:', newState)
     return newState
-
 }
