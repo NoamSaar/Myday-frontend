0362edd2a52915a0bc1bb53390a546d6f0bdf549
Noam Saar <noamsaar11@gmail.com>
2024-01-03 17:31:30 +0200

    Fix fonts path

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
-import addIcon from '../../public/icons/Work.svg'
 
 export function HomePage() {
 
     return (
         <section className='home-page'>
             <h1>Home Page</h1>
-            <img src={addIcon}></img>
         </section >
     )
 }
