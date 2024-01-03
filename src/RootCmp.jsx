be69f6e2795d949468fb1e414e5148f847a3d8d4
Mor Marzan <mormarzan@gmail.com>
2024-01-03 20:05:10 +0200

    Fix branches issue

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -13,7 +13,7 @@ export function RootCmp() {
         <div>
             <AppHeader />
             <main>
-                {/* home */}
+                {/* home! */}
                 {/* board app */}
                 <Sidebar />
                 <Routes>
