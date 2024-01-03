a48b59a805b6c1a49737813ac269e3ce4c8045a3
Noam Saar <noamsaar11@gmail.com>
2024-01-03 16:31:28 +0200

    Change icons folder location and add font clrs

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
-
+import addIcon from '../../public/icons/Work.svg'
 
 export function HomePage() {
 
     return (
         <section className='home-page'>
             <h1>Home Page</h1>
-
+            <img src={addIcon}></img>
         </section >
     )
 }
