613f900280f1ef624f9f72cf08d83a69fc9232ac
EdenRize <edenrize@gmail.com>
2024-01-04 22:58:50 +0200

    Add toggle menu modal

--- src/cmps/TaskPreview.jsx
+++ src/cmps/TaskPreview.jsx
@@ -8,6 +8,8 @@ import { useSelector } from "react-redux";
 
 export function TaskPreview({ task, groupId }) {
     const [currTask, setCurrTask] = useState(null)
+    const [isShowMenu, setIsShowMenu] = useState(false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     const board = useSelector((storeState) => storeState.boardModule.currBoard)
 
     useEffect(() => {
@@ -43,6 +45,18 @@ export function TaskPreview({ task, groupId }) {
         }
     }
 
+    function handleMouseEnter() {
+        setIsShowMenu(true)
+    }
+
+    function handleMouseLeave() {
+        setIsShowMenu(false)
+    }
+
+    function toggleMenu() {
+        setIsMenuOpen(prevIsOpen => !prevIsOpen)
+    }
+
     const menuOptions = [
         {
             icon: '../../../public/icons/delete.svg',
@@ -54,19 +68,17 @@ export function TaskPreview({ task, groupId }) {
 
     if (!currTask) return <ul>Loading</ul>
     return (
-        <ul className="clean-list task-preview-container sticky-left">
+        <ul className="clean-list task-preview-container sticky-left" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+
             <div className="menu-container">
-                <MenuOptionsModal options={menuOptions} />
-                <img className="btn" src="../../../public/icons/menu.svg" />
+                {isMenuOpen && <MenuOptionsModal options={menuOptions} />}
+                {isShowMenu && <img className="btn" src="../../../public/icons/menu.svg" onClick={toggleMenu} />}
             </div>
             <ul className="clean-list task-preview" key={currTask.id}>
-
                 <div className="sticky-left task-title-container">
-
                     <li className="task-selection">
                         <input type="checkbox" />
                     </li>
-
                     <li className="task-title">{currTask.title}</li>
                 </div>
 
@@ -81,30 +93,3 @@ export function TaskPreview({ task, groupId }) {
     )
 }
 
-// export function TaskPreview({ task }) {
-//     //GET FROM STORE
-//     const cmpsOrder = [
-//         "status-picker",
-//         "member-picker",
-//         "date-picker",
-//         "priority-picker",
-//     ]
-//     return (
-//         <section>
-//             <h5>{task.txt}</h5>
-//             {cmpsOrder.map((cmp, idx) => {
-//                 return (
-//                     <DynamicPicker
-//                         cmp={cmp}
-//                         key={idx}
-//                         onUpdate={(data) => {
-//                             console.log("Updating: ", cmp, "with data:", data)
-//                             // make a copy, update the task, create an action
-//                             // Call action: updateTask(task, action)
-//                         }}
-//                     />
-//                 )
-//             })}
-//         </section>
-//     )
-// }
