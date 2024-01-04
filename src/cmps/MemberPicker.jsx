7b7621560d93a8f51192a32ed068c9064a5ce159
EdenRize <edenrize@gmail.com>
2024-01-04 14:02:35 +0200

    Fix extra members in members picker

--- src/cmps/MemberPicker.jsx
+++ src/cmps/MemberPicker.jsx
@@ -8,9 +8,11 @@ export function MemberPicker({ members, onUpdate }) {
 
             {!!members.length && <div className="member-img-container">
                 {members.map((member, idx) => {
-                    return member.startsWith('http') ?
+                    return idx < 2 ? member.startsWith('http') ?
                         <img key={idx} src={member} /> :
                         <span key={idx} className="extra-members-box">{member.split(' ')[0][0] + member.split(' ')[1][0]}</span>
+                        :
+                        ''
                 })}
                 {extraMembers > 0 && <span className="extra-members-box">+{extraMembers}</span>}
             </div>
