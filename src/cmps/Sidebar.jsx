db805e19bef501dd64484722e8e0d6cc4d7e3380
Noam Saar <noamsaar11@gmail.com>
2024-01-04 13:23:37 +0200

    Add btn mixin

--- src/cmps/Sidebar.jsx
+++ src/cmps/Sidebar.jsx
@@ -12,19 +12,43 @@ export function Sidebar() {
     return (
         <section className="sidebar-container">
             <article className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-                <div className="sidebar-nav">
-                    <NavLink to="/" className="sidebar-btn flex align-center" >
+                <nav className="sidebar-main-nav">
+                    <NavLink to="/" className="btn" >
                         <img src="../../public/icons/Home.svg" alt="home-icon" />
                         <span>Home</span>
                     </NavLink>
 
-                    <NavLink to="/board" className="sidebar-btn flex justify-center align-center" >
+                    <NavLink to="/board" className="btn" >
                         <img src="../../public/icons/Calendar.svg" alt="home-icon" />
                         <span>My Work</span>
                     </NavLink>
                     <button className="btn-menu" onClick={onOpenSidebar} title="menu-btn">x</button>
-                </div>
-                <nav className="app-nav">
+                </nav>
+
+                <section className="sidebar-workspace">
+                    <div className="workspace-section">
+                        <div className="workspace-logo">
+                            M
+                            <div className="btn">
+                                <img src="../../public/icons/Home.svg" alt="home-icon" />
+                            </div>
+                        </div>
+                        <span>Main workspace</span>
+                    </div>
+                    <div className="sidebar-filter">
+                        <div className="search-section">
+                            <div className="btn">
+                                <img src="../../public/icons/Search.svg" alt="search-icon" />
+                            </div>
+                            <input type="text" />
+                        </div>
+                        <button className="btn clrblue">
+                            <img src="../../public/icons/AddSmall.svg" alt="add-icon" />
+                        </button>
+                    </div>
+                </section>
+
+                <nav className="sidebar-board-nav">
                 </nav>
             </article>
         </section>
