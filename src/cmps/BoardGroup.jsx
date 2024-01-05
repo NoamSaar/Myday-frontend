1aa2a537f7555e389ba0e1f22500838acf925586
EdenRize <edenrize@gmail.com>
2024-01-05 12:46:58 +0200

    Move css properties for group header

--- src/cmps/BoardGroup.jsx
+++ src/cmps/BoardGroup.jsx
@@ -40,15 +40,21 @@ export function BoardGroup({ group, titlesOrder }) {
 
     return (
         <section className='board-group'>
-            <div className="board-title-container sticky-left" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <div className="menu-container">
-                    {isMenuOpen && <MenuOptionsModal options={menuOptions} />}
-                    <img className="btn" src="../../../public/icons/menu.svg" onClick={toggleMenu} />
+            <div className="group-sticky-container sticky-left">
+
+                <div className="board-title-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    <div className="menu-container">
+                        {isMenuOpen && <MenuOptionsModal options={menuOptions} />}
+                        <img className="btn" src="../../../public/icons/menu.svg" onClick={toggleMenu} />
+                    </div>
+                    <img className="down-arrow" src="../../../public/icons/NavigationChevronDown.svg" title="Collapse group" />
+                    <h4 >{group.title}</h4>
+                    <p>{group.tasks.length} Tasks</p>
                 </div>
-                <img className="down-arrow" src="../../../public/icons/NavigationChevronDown.svg" title="Collapse group" />
-                <h4 >{group.title}</h4>
-                <p>{group.tasks.length} Tasks</p>
+
+
             </div>
+
             <TaskList titlesOrder={titlesOrder} groupId={group.id} />
         </section>
     )
