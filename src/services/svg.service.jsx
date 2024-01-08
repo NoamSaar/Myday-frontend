8a73b743421347e0091aa4e4b503a39a40e482f0
EdenRize <edenrize@gmail.com>
2024-01-08 18:25:08 +0200

    Add autofocus to group title when adding new group

--- src/services/svg.service.jsx
+++ src/services/svg.service.jsx
@@ -120,6 +120,17 @@ export function AngleDownIcon() {
     </svg>
   )
 }
+export function AngleDownBoldIcon() {
+  return (
+    <svg width="800px" height="800px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke="currentColor" stroke-width="0.64">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+      <g id="SVGRepo_iconCarrier">
+        <path fillRule="evenodd" d="M12.2929,5.292875 C12.6834,4.902375 13.3166,4.902375 13.7071,5.292875 C14.0976,5.683375 14.0976,6.316555 13.7071,6.707085 L8.70711,11.707085 C8.31658,12.097605 7.68342,12.097605 7.29289,11.707085 L2.29289,6.707085 C1.90237,6.316555 1.90237,5.683375 2.29289,5.292875 C2.68342,4.902375 3.31658,4.902375 3.70711,5.292875 L8,9.585765 L12.2929,5.292875 Z" /> </g>
+    </svg>
+
+  )
+}
 export function AngleRightIcon() {
   return (
     <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
