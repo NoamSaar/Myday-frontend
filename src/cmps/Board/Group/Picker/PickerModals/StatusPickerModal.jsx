a86cc4fe66c6f00b7482de511f338a63947e8f4f
Mor Marzan <mormarzan@gmail.com>
2024-01-08 09:04:27 +0200

    Add style status picker

--- src/cmps/Board/Group/Picker/PickerModals/StatusPickerModal.jsx
+++ src/cmps/Board/Group/Picker/PickerModals/StatusPickerModal.jsx
@@ -6,19 +6,38 @@ export function StatusPickerModal({ selectedStatus, onChangeStatus }) {
     const { status: statuses } = useSelector((storeState) => storeState.boardModule.currBoard)
     const [statusToEdit, setStatusToEdit] = useState(selectedStatus)
 
-    function handleChange({ target }) {
-        let { value } = target
-        setStatusToEdit(value)
-        onChangeStatus('status', value) //important to pass the value becuse the statusToEdit will update only on nxt render
+    function handleChange(newStatus) {
+        // let { value } = target
+        setStatusToEdit(newStatus)
+        onChangeStatus('status', newStatus) //important to pass the value becuse the statusToEdit will update only on nxt render
     }
+    // function handleChange({ target }) {
+    //     let { value } = target
+    //     setStatusToEdit(value)
+    //     onChangeStatus('status', value) //important to pass the value becuse the statusToEdit will update only on nxt render
+    // }
 
     return (
-        <div className="status-picker-modal">
-            <select value={statusToEdit} name="status" onChange={handleChange}>
+        <div className="general-modal status-picker-modal">
+            {/* <select value={statusToEdit} name="status" onChange={handleChange}>
                 {statuses.map(status => (
                     <option key={status.id} value={status.title}>{status.title}</option>
                 ))}
-            </select>
+            </select> */}
+
+            <ul className='clean-list manual-select'>
+                {statuses.map(status => (
+                    <li className='manual-option btn'
+                        onClick={() => handleChange(status.title)}
+                        style={{ backgroundColor: status.color }}
+                        key={status.id} value={status.title || ''}
+                    >
+                        <span>
+                            {status.title || ''}
+                        </span>
+                    </li>
+                ))}
+            </ul>
 
         </div>
     )
