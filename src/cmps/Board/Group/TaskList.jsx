9f3993ba734c3d44ae38123a7c0ea850d06829cc
EdenRize <edenrize@gmail.com>
2024-01-09 23:41:29 +0200

    Init dnd with id

--- src/cmps/Board/Group/TaskList.jsx
+++ src/cmps/Board/Group/TaskList.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux"
 import { TaskPreview } from "./TaskPreview"
 import { AddTask } from "./AddTask"
 import { useState } from "react"
-import { addTask, setActiveTask, updateBoard } from "../../../store/actions/board.actions"
+import { addTask, getBoardById, setActiveTask, updateBoard } from "../../../store/actions/board.actions"
 
 export function TaskList({ groupId, groupColor, highlightText, filterBy }) {
     const board = useSelector((storeState) => storeState.boardModule.currBoard)
@@ -12,16 +12,50 @@ export function TaskList({ groupId, groupColor, highlightText, filterBy }) {
     const groupIdx = board.groups.findIndex(group => group.id === groupId)
     const group = board.groups[groupIdx]
 
-    const handleDragEnd = (result) => {
-        if (!result.destination) return
+    const handleDragEnd = async (result) => {
+        try {
+            if (!result.destination) return
+            const fullBoard = await getBoardById(board._id)
+            console.log('fullBoard', fullBoard)
 
-        const newOrderedTasks = group.tasks
-        const [removed] = newOrderedTasks.splice(result.source.index, 1)
-        newOrderedTasks.splice(result.destination.index, 0, removed)
-        saveNewOrder()
+            // const newOrderedTasks = group.tasks
+
+            const filteredSourceGroupIdx = board.groups.findIndex(group => group.id === result.source.droppableId)
+            const filteredDestinationGroupIdx = board.groups.findIndex(group => group.id === result.destination.droppableId)
+            const filteredSourceGroupTasks = board.groups[filteredSourceGroupIdx].tasks
+            const filteredDestinationGroupTasks = board.groups[filteredDestinationGroupIdx].tasks
+            console.log('filteredSourceGroupTasks', filteredSourceGroupTasks)
+            console.log('filteredDestinationGroupTasks', filteredDestinationGroupTasks)
+
+            const trueSourceGroupIdx = fullBoard.groups.findIndex(group => group.id === result.source.droppableId)
+            const trueDestinationGroupIdx = fullBoard.groups.findIndex(group => group.id === result.destination.droppableId)
+            const trueSourceGroupTasks = fullBoard.groups[trueSourceGroupIdx].tasks
+            const trueDestinationGroupTasks = fullBoard.groups[trueDestinationGroupIdx].tasks
+            console.log('trueSourceGroupTasks', trueSourceGroupTasks)
+            console.log('trueDestinationGroupTasks', trueDestinationGroupTasks)
+
+            const sourceTaskId = filteredSourceGroupTasks[result.source.index].id
+            const destinationTaskId = filteredDestinationGroupTasks[result.destination.index].id
+            console.log('sourceTaskId', sourceTaskId)
+            console.log('destinationTaskId', destinationTaskId)
+
+
+            const sourceTaskIdx = trueSourceGroupTasks.findIndex(task => task.id === sourceTaskId)
+            const destinationTaskIdx = trueDestinationGroupTasks.findIndex(task => task.id === destinationTaskId)
+            console.log('sourceTaskIdx', sourceTaskIdx)
+            console.log('destinationTaskIdx', destinationTaskIdx)
+
+
+            const [removed] = trueSourceGroupTasks.splice(sourceTaskIdx, 1)
+            trueDestinationGroupTasks.splice(destinationTaskIdx, 0, removed)
+            saveNewOrder(fullBoard, filteredSourceGroupIdx, fullBoard.groups[filteredSourceGroupIdx])
+            saveNewOrder(fullBoard, filteredDestinationGroupIdx, fullBoard.groups[filteredDestinationGroupIdx])
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    async function saveNewOrder() {
+    async function saveNewOrder(board, groupIdx, group) {
         try {
             const newBoard = { ...board }
             newBoard.groups.splice(groupIdx, 1, group)
@@ -98,36 +132,3 @@ export function TaskList({ groupId, groupColor, highlightText, filterBy }) {
         </ul>
     )
 }
-
-// async function handleDragEnd(result) {
-//     try {
-
-//         const fullBoard = await getBoardById(board._id)
-//         console.log('fullBoard', fullBoard)
-//         const initGroupIdx = fullBoard.groups.findIndex(group => group.id === groupId)
-//         const initGroup = fullBoard.groups[initGroupIdx]
-//         const newOrderedTasks = initGroup.tasks
-//         console.log('newOrderedTasks', newOrderedTasks)
-
-//         const [removed] = newOrderedTasks.splice(result.source.index, 1)
-//         newOrderedTasks.splice(result.destination.index, 0, removed)
-//         console.log('initGroup.tasks', initGroup.tasks)
-
-//         saveNewOrder(fullBoard, initGroupIdx, initGroup)
-//         if (!result.destination) return
-//     } catch (err) {
-//         console.log('Cannot move task:', err)
-
-//     }
-
-// }
-
-
-// async function saveNewOrder(newBoard, groupIdx, group) {
-//     try {
-//         newBoard.groups.splice(groupIdx, 1, group)
-//         await updateBoard(newBoard)
-//     } catch (err) {
-//         console.log('Cannot save group:', err)
-//     }
-// }
