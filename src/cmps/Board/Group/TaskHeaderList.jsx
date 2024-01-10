3f659d3d3ac71795396f361d17c4994e6421faf7
EdenRize <edenrize@gmail.com>
2024-01-10 14:11:57 +0200

    Add subgrid to table header

--- src/cmps/Board/Group/TaskHeaderList.jsx
+++ src/cmps/Board/Group/TaskHeaderList.jsx
@@ -2,23 +2,27 @@ import { GroupTitlesList } from "./GroupTitlesList";
 
 export function TaskHeaderList({ groupColor, titlesOrder }) {
     return (
-        <div className="task-header-list-container sticky-left flex">
+        <div className="task-header-list-container">
 
 
             <ul className="clean-list task-header-list sticky-left">
-                <div className="task-row-placeholder sticky-left"></div>
-                <div style={{ backgroundColor: groupColor }} className="color-display sticky-left-36"></div>
+                <div className="task-title-sticky flex sticky-left">
+                    <div className="task-row-placeholder sticky-left"></div>
+                    <div style={{ backgroundColor: groupColor }} className="color-display sticky-left-36"></div>
 
-                <div className="task-title-container">
-                    <li className="task-selection">
-                        <input type="checkbox" />
-                    </li>
+                    <div className="task-title-container">
+                        <li className="task-selection">
+                            <input type="checkbox" />
+                        </li>
 
-                    <li className="task-title">Task</li>
+                        <li className="task-title">Task</li>
+                    </div>
                 </div>
 
-                <GroupTitlesList titles={titlesOrder} />
-                <li className="line-end"></li>
+                <div className="titles-content">
+                    <GroupTitlesList titles={titlesOrder} />
+                    <li className="line-end"></li>
+                </div>
             </ul>
         </div>
     )
