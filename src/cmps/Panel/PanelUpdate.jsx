9bf1e0f2832eafac7446b6235f7323f843cc518b
Noam Saar <noamsaar11@gmail.com>
2024-01-10 22:24:39 +0200

    Fix className in PanelUpdate

--- src/cmps/Panel/PanelUpdate.jsx
+++ src/cmps/Panel/PanelUpdate.jsx
@@ -6,11 +6,11 @@ export function PanelUpdate({ updates }) {
             {updates.length > 0 ?
                 updates[0].txt
                 :
-                <div class="post_not_found grid place-center">
+                <div className="post_not_found grid place-center">
                     <img src="../../../public/icons/no-updates.svg" alt="" />
                     <div className="post-not-found-txt">
                         <h2>No updates yet for this item</h2>
-                        <p class="post_not_found_subtitle">Be the first one to update about progress, mention someone
+                        <p className="post_not_found_subtitle">Be the first one to update about progress, mention someone
                             <br />
                             or upload files to share with your team members</p>
                     </div>
