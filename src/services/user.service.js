9aeef6407f375d6c7f18f35eb9bad0a29e173950
Noam Saar <noamsaar11@gmail.com>
2024-01-11 23:18:46 +0200

    Refactor user.service to use back

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,10 @@
 import { storageService } from './async-storage.service'
 import { httpService } from './http.service'
 
+// const STORAGE_KEY = 'userDB'
 const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
-const STORAGE_KEY = 'userDB'
-
+const USER_BASE_URL = 'user/'
+const AUTH_BASE_URL = 'auth/'
 
 export const userService = {
     login,
@@ -19,59 +20,33 @@ export const userService = {
 
 window.userService = userService
 
-const gUsers = [
-    {
-        _id: 'u101',
-        fullname: 'Eden Rize',
-        username: 'edenrize@gmail.com',
-        password: '1234',
-        imgUrl: 'https://res.cloudinary.com/dkvliixzt/image/upload/v1704360556/catt_dytsgq.jpg',
+// const gUsers = getUsers()
 
-    },
-    {
-        _id: 'u102',
-        fullname: 'Mor Marzan',
-        username: 'mormarzan@gmail.com',
-        password: '4321',
-
-    },
-    {
-        _id: 'u103',
-        fullname: 'Noam Saar',
-        username: 'noamsaar11@gmail.com',
-        password: '4321',
-        imgUrl: 'https://res.cloudinary.com/dkvliixzt/image/upload/v1704360614/noam_tdr2rp.jpg',
-
-    },
-]
-
-_initUsers()
+// _initUsers()
 
 async function getUsers() {
     try {
-        return await storageService.query(STORAGE_KEY)
+        // return await storageService.query(STORAGE_KEY)
+        return httpService.get(USER_BASE_URL)
     } catch (err) {
         throw new Error(err.message || 'An err occurred during getting boards')
     }
-    // return httpService.get(`user`)
 }
-
-
 
 async function getById(userId) {
     try {
-        const user = await storageService.get(STORAGE_KEY, userId)
-        // const user = await httpService.get(`user/${userId}`)
+        // const user = await storageService.get(STORAGE_KEY, userId)
+        const user = await httpService.get(USER_BASE_URL + userId)
         return user
     } catch (err) {
         throw new Error(err.message || 'An err occurred during getting user')
     }
 }
 
-function remove(userId) {
+async function remove(userId) {
     try {
-        return storageService.remove(STORAGE_KEY, userId)
-        // return httpService.delete(`user/${userId}`)
+        // return storageService.remove(STORAGE_KEY, userId)
+        return await httpService.delete(USER_BASE_URL + userId)
     } catch (err) {
         throw new Error(err.message || 'An err occurred during removing user')
     }
@@ -80,8 +55,8 @@ function remove(userId) {
 async function save(user) {
     try {
         if (user._id) {
-            return await storageService.put(STORAGE_KEY, user)
-            // const user = await httpService.put(`user/${_id}`, user)
+            // return await storageService.put(STORAGE_KEY, user)
+            return await httpService.put(USER_BASE_URL + user._id, user)
         } else {
             return await storageService.post(STORAGE_KEY, user)
         }
@@ -94,9 +69,11 @@ async function save(user) {
     // if (getLoggedinUser()._id === user._id) saveLocalUser(user)
 }
 
+// auth 
+
 async function login(userCred) {
     try {
-        const user = await httpService.post('auth/login', userCred)
+        const user = await httpService.post(AUTH_BASE_URL + 'login', userCred)
         if (user) return saveLocalUser(user)
     } catch (err) {
         throw new Error(err.message || 'An err occurred during login')
@@ -110,7 +87,7 @@ async function signup(userCred) {
     // if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
     // const user = await storageService.post('user', userCred)
 
-    const user = await httpService.post('auth/signup', userCred)
+    const user = await httpService.post(AUTH_BASE_URL + 'signup', userCred)
     return saveLocalUser(user)
 }
 
@@ -119,7 +96,7 @@ async function logout() {
     // return await httpService.post('auth/logout')
 }
 
-
+// session storage
 
 function saveLocalUser(user) {
     user = { _id: user._id, fullname: user.fullname, imgUrl: user.imgUrl }
@@ -130,20 +107,3 @@ function saveLocalUser(user) {
 function getLoggedinUser() {
     return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
 }
-
-
-// ;(async ()=>{
-//     await userService.signup({fullname: 'Puki Norma', username: 'puki', password:'123',score: 10000, isAdmin: false})
-//     await userService.signup({fullname: 'Master Adminov', username: 'admin', password:'123', score: 10000, isAdmin: true})
-//     await userService.signup({fullname: 'Muki G', username: 'muki', password:'123', score: 10000})
-// })()
-
-
-function _initUsers() {
-    const usersFromStorage = localStorage.getItem(STORAGE_KEY)
-    if (usersFromStorage && usersFromStorage.length) return
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(gUsers))
-}
-
-
