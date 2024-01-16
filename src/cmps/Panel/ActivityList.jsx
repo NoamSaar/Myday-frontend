2a7dec15f2af8755b2fbf42130e4610d2554cacc
Noam Saar <noamsaar11@gmail.com>
2024-01-16 23:10:00 +0200

    Add desc sort in ActivityList

--- src/cmps/Panel/ActivityList.jsx
+++ src/cmps/Panel/ActivityList.jsx
@@ -2,10 +2,12 @@
 import { ActivityPreview } from "./ActivityPreview.jsx";
 
 export function ActivityList({ activities }) {
+    const sortedActivities = [...activities].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+
     return (
         <ul className="activity-list clean-list" >
             {
-                activities.map(activity =>
+                sortedActivities.map(activity =>
                     <li className="activity-preview subgrid full-grid-column align-center" key={activity._id} >
                         <ActivityPreview activity={activity} />
                     </li>
