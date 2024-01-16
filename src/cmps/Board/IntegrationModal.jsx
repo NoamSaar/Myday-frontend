4b37b5ea24909c45b1fb93912ba792162bad8de7
EdenRize <edenrize@gmail.com>
2024-01-16 20:40:39 +0200

    Add google btn css

--- src/cmps/Board/IntegrationModal.jsx
+++ src/cmps/Board/IntegrationModal.jsx
@@ -1,4 +1,5 @@
 import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react'
+import { GoogleBtn } from './GoogleBtn'
 
 export function IntegrationModal() {
     const session = useSession() //tokens, when session exists we have a user
@@ -29,59 +30,12 @@ export function IntegrationModal() {
         }
     }
 
-    async function createCalendarEvent() {
-        try {
-            const event = {
-                'summary': 'event name',
-                'description': 'Descriptionnnn',
-                'start': {
-                    'dateTime': new Date().toISOString(),
-                    'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
-                },
-                'end': {
-                    'dateTime': new Date().toISOString(),
-                    'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
-                }
-            };
-
-            const response = await fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
-                method: 'post',
-                headers: {
-                    'Authorization': `Bearer ${session.provider_token}`,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(event),
-            });
-
-            if (!response.ok) {
-                throw new Error(`Failed to create calendar event. Status: ${response.status}`);
-            }
-
-            const data = await response.json();
-            console.log('data', data);
-            console.log('event created!');
-        } catch (err) {
-            console.error('Error creating calendar event:', err);
-        }
-    }
-
-    // console.log('session.user', session.user)
-
     if (isLoading) return <div className="integration-modal">Loading...</div>
-    // createCalendarEvent()
     return (
         <div className="integration-modal">
             <h1>Integrations</h1>
-            {session ?
-                <>
-                    <h2>Hey {session.user.email}</h2>
-                    <button onClick={() => signOut()}>Sign out of google</button>
-                </>
-                :
-                <>
-                    <button onClick={() => googleSignIn()}>Sign in with google</button>
-                </>
-            }
+            {session && <h2>Hey {session.user.email}</h2>}
+            <GoogleBtn onBtnClick={session ? () => signOut() : () => googleSignIn()} txt={session ? 'Sign out of google' : 'Sign in with google'} />
         </div>
     )
 }
