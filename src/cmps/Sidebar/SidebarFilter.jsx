291780e32a5f5a2ebaada3664362f11cd5cfdaf1
Mor Marzan <mormarzan@gmail.com>
2024-01-17 18:50:19 +0200

    Add tooltips to sidebar

--- src/cmps/Sidebar/SidebarFilter.jsx
+++ src/cmps/Sidebar/SidebarFilter.jsx
@@ -1,14 +1,20 @@
 import { useRef, useState } from "react"
+import { useSelector } from "react-redux"
 
 import { utilService } from "../../services/util.service"
 
 import { DynamicInput } from "../DynamicInput"
 import { useEffectUpdate } from "../../customHooks/useEffectUpdate"
 import { PlusIcon, FilterIcon } from "../../services/svg.service"
+import { resetDynamicModal, setDynamicModal } from "../../store/actions/system.actions"
 
 export function SidebarFilter({ filterBy, onSetFilter, onAddNewBoard }) {
     onSetFilter = useRef(utilService.debounce(onSetFilter))
     const [filterByToEdit, setFilterByToEdit] = useState({ ...filterBy })
+
+    const { parentId, type, isOpen } = useSelector((storeState) => storeState.systemModule.dynamicModal)
+    const isModalOpen = parentId === `add-board-tooltip`
+    const titleRef = useRef(null)
 
     useEffectUpdate(() => {
         onSetFilter.current(filterByToEdit)
@@ -18,6 +24,28 @@ export function SidebarFilter({ filterBy, onSetFilter, onAddNewBoard }) {
         let { value, name: field, type } = target
         value = type === 'number' ? +value : value
         setFilterByToEdit((prevFilter) => ({ ...prevFilter, [field]: value }))
+    }
+
+    function onStatEnter(txt, name) {
+        if (isOpen && type !== 'tooltip') return
+
+        setDynamicModal(
+            {
+                isOpen: true,
+                parentRefCurrent: titleRef.current,
+                type: 'tooltip',
+                data: { txt },
+                parentId: `${name}-tooltip`,
+                hasCaret: true,
+                isCenter: true,
+                isPosBlock: true,
+                caretClred: true
+
+            })
+    }
+
+    function onStatLeave() {
+        if (isModalOpen) resetDynamicModal()
     }
 
     const inputProps = {
@@ -42,8 +70,14 @@ export function SidebarFilter({ filterBy, onSetFilter, onAddNewBoard }) {
             <div className="sidebar-filter grid column place-center">
                 <DynamicInput inputProps={inputProps} />
             </div>
-            <button className="btn clrblue" title="Add Item to Workspace"
-                onClick={onAddNewBoard}>
+            <button
+                className="btn clrblue"
+                // title="Add Item to Workspace"
+                onClick={onAddNewBoard}
+                ref={titleRef}
+                onMouseEnter={() => onStatEnter('Add Item to workspace', 'add-board')}
+                onMouseLeave={onStatLeave}
+            >
                 <PlusIcon />
             </button>
         </>
